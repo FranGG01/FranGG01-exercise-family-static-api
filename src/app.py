e786aec51335b187f23b457d8a9ab8c8ffa77b33
Francisco García <g.garciafran201@gmail.com>
2025-06-11 17:37:35 +0000

    Proyecto acabado

--- src/app.py
+++ src/app.py
@@ -35,7 +35,27 @@ def handle_hello():
     members = jackson_family.get_all_members()
     response_body = {"hello": "world",
                      "family": members}
-    return jsonify(response_body), 200
+    return jsonify(members), 200
+
+@app.route('/members', methods=['POST'])
+def add_member():
+    # This is how you can use the Family datastructure by calling its methods
+    member = request.get_json()
+    member_update = jackson_family.add_member(member)
+    return jsonify(member_update), 200
+
+@app.route('/members/<int:id>', methods=['GET'])
+def get_member(id):
+    # This is how you can use the Family datastructure by calling its methods
+    member = jackson_family.get_member(id)
+    return jsonify(member), 200
+   
+@app.route('/members/<int:id>', methods=['DELETE'])
+def delete_member(id):
+    # This is how you can use the Family datastructure by calling its methods
+    jackson_family.delete_member(id)
+    return jsonify({'done':True}), 200
+   
 
 
 
